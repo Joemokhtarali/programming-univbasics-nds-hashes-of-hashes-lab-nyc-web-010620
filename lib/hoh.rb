9757ef6835f439596bca0d41a93f990f7b00d9c2
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-25 17:14:25 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,35 +32,18 @@
 
 def naming_system
   hash = {
-    PORTION_1 = {
-     label: "Kingdom",
+      :Kingdom=> {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
       sub_category: {
-        label: "Phylum",
-          sub_category: {
-            label: "Class"
-    }
-  }
-}
-
-    PORTION_2 = {
-      label: "Order"
-  }
-
-    PORTION_3 = {
-      label: "Family",
-      sub_category: {
-        label: "Genus",
+        label: "Class"
       }
     }
-    
-    PORTION_4 = {
-      label: "Species",
-      sub_category: nil
-    }
-}
+  }
   
-  # Remember:
-  #  Kingdom
+    # Remember:
+  #  Kingdom 
   #  Phylum
   #  Class
   #  Order
